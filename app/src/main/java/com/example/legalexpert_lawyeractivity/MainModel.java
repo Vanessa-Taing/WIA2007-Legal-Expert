dad31216190261ae8678fb6cc8f3a02d0621e294
Vanessa-Taing <22004847@siswa.um.edu.my>
2023-12-03 12:51:19 +0800

    hmm

---
+++ app/src/main/java/com/example/legalexpert_lawyeractivity/MainModel.java
@@ -0,0 +1,111 @@
+package com.example.legalexpert_lawyeractivity;
+
+public class MainModel {
+    String name, lawFirm, doB, expYear, specialization, gender, language, state, mobile, email, rating ;
+
+
+    MainModel(){
+
+    }
+    public MainModel(String name, String lawFirm, String doB, String expYear, String specialization, String gender, String language, String state, String mobile, String email, String rating) {
+        this.name = name;
+        this.lawFirm = lawFirm;
+        this.doB = doB;
+        this.expYear = expYear;
+        this.specialization = specialization;
+        this.gender = gender;
+        this.language = language;
+        this.state = state;
+        this.mobile = mobile;
+        this.email = email;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public String getLawFirm() {
+        return lawFirm;
+    }
+
+    public void setLawFirm(String lawFirm) {
+        this.lawFirm = lawFirm;
+    }
+
+    public String getDoB() {
+        return doB;
+    }
+
+    public void setDoB(String doB) {
+        this.doB = doB;
+    }
+
+    public String getExpYear() {
+        return expYear;
+    }
+
+    public void setExpYear(String expYear) {
+        this.expYear = expYear;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
